c61a967f60510ddebe632bcd74f80485f5610342
DarkSideShadows <brylejaronong@gmail.com>
2025-02-28 15:00:36 -0800

    initial commit
    server and client working

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+#import pickle # serialize object (pickle), send through socket to server, server unpickles and uses object
+
+# constants
+HEADER = 64 # 64 bytes to determine length of message
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname()) # local ip address
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE ="!DISCONNECT" # for clean disconnects (concern: false positive user is connected)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # setup socket for client
+client.connect(ADDR) # connect to server
+
+# different send formats: json string? pickle?
+def send(obj):
+    pass
+
+def send(msg): # send a string
+    message = msg.encode(FORMAT) # encode string into a byte object to send through socket
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT) 
+    send_length += b" " * (HEADER - len(send_length)) # pad message to get 64 bytes
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT)) # handle whatever message is sent back from server
+
+send("Hello World!")
+input()
+send("Hello everyone!")
+input()
+send("Hello Tim!")
+send(DISCONNECT_MESSAGE)
---
+++ server.py
@@ -0,0 +1,49 @@
+import socket
+import threading
+
+# constants
+HEADER = 64 # 64 bytes to determine length of message
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname()) # local ip address
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE ="!DISCONNECT" # for clean disconnects (concern: false positive user is connected)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # open device to other connections, send data through stream
+server.bind(ADDR) # anything that connects to this server goes to ADDR
+
+# handle individual connection between server and client
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        # wait to rececive information from client
+        # do something with information
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length: # check for valid message
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+
+            # send message from server to client
+            conn.send("Message recieved.".encode(FORMAT))
+    
+    conn.close() # close current connection
+
+def start():
+   server.listen() # listen to connections
+   print(f"[LISTENING] Server is listening on {SERVER}")
+   while True: 
+       # wait for new connection
+       # information: ip addr, port (addr) & connection object (conn)
+       conn, addr = server.accept() 
+       thread = threading.Thread(target=handle_client, args=(conn, addr))
+       thread.start()
+       print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
+
+print("[STARTING] server is starting...")
+start()
