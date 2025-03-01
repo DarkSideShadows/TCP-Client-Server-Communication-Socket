5ec96d58fb4fc4ad4946081ecc5ebf17bd85a5f2
Gigi <145626149+sneasxl@users.noreply.github.com>
2025-02-28 18:30:25 -0800

    Update server.py
    edited handle_client and added encode_message

--- server.py
+++ server.py
@@ -12,27 +12,30 @@ DISCONNECT_MESSAGE ="!DISCONNECT" # for clean disconnects (concern: false positi
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # open device to other connections, send data through stream
 server.bind(ADDR) # anything that connects to this server goes to ADDR
 
-# handle individual connection between server and client
+def encode_message(message):
+    return ''.join(chr(ord(char) + 1) for char in message)
+
+# Handle individual client connections
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
 
     connected = True
     while connected:
-        # wait to rececive information from client
-        # do something with information
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length: # check for valid message
+        if msg_length:  # Check for a valid message
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
+
             if msg == DISCONNECT_MESSAGE:
-                connected = False
+                connected = False  # Disconnect if user sends "!DISCONNECT"
 
             print(f"[{addr}] {msg}")
 
             # send message from server to client
-            conn.send("Message recieved.".encode(FORMAT))
-    
-    conn.close() # close current connection
+            encoded_msg = encode_message(msg)
+            conn.send(encoded_msg.encode(FORMAT))
+
+    conn.close()  # Close the connection
 
 def start():
    server.listen() # listen to connections
